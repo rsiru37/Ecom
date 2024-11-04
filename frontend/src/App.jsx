a5d7b4ccf134b1e318309aac0b03e111792176d4
Raj-37 <rajsiruvani@gmail.com>
2024-11-04 14:45:39 +0530

    Modifying The home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,15 +8,18 @@ import { ADashboard } from './components/ADashboard';
 import {Payments} from "./components/Payments"
 function App() {
   return (
+    <>
     <BrowserRouter>
       <Routes>
-        <Route path={"/signup"} element={<Signup />} />
+        <Route path={"/signup"} element={<Signup />}/>
         <Route path={"/signin"} element={<Signin />} />
         <Route path={"/dashboard"} element={<Dashboard />} />
         <Route path={"/admin-board"} element={<ADashboard />} />
         <Route path={"/pay"} element={<Payments/>}/>
       </Routes>
     </BrowserRouter>
+    <h1>Welcome to Shopee App</h1>
+    </>
   )
 }
 
